be79263566dc6b8ae75a53af9b84073b918300ec
Arty <arthur.bergbauer@viacesi.fr>
2024-03-23 16:39:45 +0100

    modification de quelques variables

--- script/profil.js
+++ script/profil.js
@@ -1,11 +1,11 @@
 var overlay = document.getElementById("overlay");
 var zoomedImage = document.getElementById("zoomedImage");
-var profilPic = document.getElementById("profilePic");
+var profilPic = document.getElementById("profilPic");
 
 profilPic.addEventListener("click", function() 
 {
     overlay.style.display = "block";
-    zoomedImage.src = profilePic.src;
+    zoomedImage.src = profilPic.src;
     zoomedImage.style.display = "block";
 });
 
