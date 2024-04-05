abd9e4fac73db639e413dfe0099b2b224fe28c9d
Arty <arthur.bergbauer@viacesi.fr>
2024-04-05 07:52:43 +0200

    Ajout des mentions légales*

--- footer.php
+++ footer.php
@@ -2,5 +2,7 @@
 <script defer src="script/footer.js"></script>
 
 <footer id="footer">
-	<nav>&copy;2024 | Tinkièt' | Tous droits réservés</nav>
+	<nav>&copy;2024 | Tinkièt' | 
+		<a href="Mentions_legales.html">Mentions Légales</a>
+	</nav>
 </footer>
